73a75c9b30110712d869fcb9ef68500e110c8eaa
Turi <iman.bulls@gmail.com>
2025-05-13 12:11:19 +0300

    ok

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaRY22Cd9LfGrgj5a0LJT-fwqgj3E7f14",
+  authDomain: "kranian-farms-kenya.firebaseapp.com",
+  projectId: "kranian-farms-kenya",
+  storageBucket: "kranian-farms-kenya.appspot.com",
+  messagingSenderId: "298881664778",
+  appId: "1:298881664778:web:5493b8f289fa0f9fee5cf3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
