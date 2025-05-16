942725af1c0455ca318b344037a45a4a687eff66
iman.bulls <iman.bulls@gmail.com>
2025-05-16 12:24:45 +0000

    Replaced "Add to Cart" with "Get a Quote"

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Product } from '@/data/products';
 import { useCart } from '@/context/CartContext';
 import { ShoppingCart } from 'lucide-react';
+import { Receipt } from 'lucide-react';
 
 type ProductCardProps = {
   product: Product;
@@ -40,7 +41,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       </Link>
       <div className="px-4 pb-4 mt-auto">
         <Button onClick={handleAddToCart} className="w-full bg-kranian-600 hover:bg-kranian-700 text-white">
-          <ShoppingCart className="mr-2 h-4 w-4" /> Add to Cart
+          <Receipt className="mr-2 h-4 w-4" /> Get a Quote
         </Button>
       </div>
     </div>
